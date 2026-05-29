e8e2d098c5cd11d65e0fbbae6c71e723077f9fa3
raj1809 <rajtheking147@gmail.com>
2026-05-29 19:55:57 +0530

    Tests on login page using getByRole, getByLabel, getByTestId

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
+  
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -28,11 +29,13 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+    testIdAttribute: 'data-test',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
+  
   /* Configure projects for major browsers */
   projects: [
     {
---
+++ tests/locators.spec.js
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test'
+
+test('Locators testing - login flow using only getByRole for username, password, and login button', async ({page}) =>{
+    await page.goto('https://www.saucedemo.com/')
+    await page.getByRole('textbox', { name : 'username'}).fill('standard_user')
+    await page.getByRole('textbox', { name : 'password'}).fill('secret_sauce')
+    await page.getByRole('button', { name : 'Login'}).click()
+
+})
+
+test('Locators using getByLabel or getByTestId', async ({page}) =>{
+
+    await page.goto('https://www.saucedemo.com/')
+    await page.getByTestId('username').fill('standard_user')
+    await page.getByTestId('password').fill('secret_sauce')
+    await page.getByTestId('login-button').click()
+    const header = await page.getByText('Products')
+    await expect(header).toBeVisible()
+    const headerText = await header.textContent()
+    console.log(headerText);
+    
+})
