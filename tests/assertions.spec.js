8b436c6bbd6937b89427206cac54d4a39fc20368
raj1809 <rajtheking147@gmail.com>
2026-05-30 18:44:42 +0530

    Completd Day 4 tests

---
+++ tests/assertions.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+
+test("Assertion tests", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await expect(page).toHaveTitle("Swag Labs");
+
+  await page.getByPlaceholder("Username").fill("standard_user");
+  await page.getByTestId("password").fill("secret_sauce");
+  await page.getByTestId("login-button").click();
+
+  await expect(page).toHaveURL(/\/inventory\.html/);
+  await expect(await page.locator("div.inventory_item_name")).toHaveCount(6);
+  await expect(
+    await page.getByTestId("add-to-cart-sauce-labs-backpack"),
+  ).toBeEnabled();
+  await expect(await page.locator("span.shopping_cart_badge")).toBeHidden();
+});
+
+test("logging in with locked_out_user / secret_sauce and assert the error message contains the right text", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.getByPlaceholder("Username").fill("locked_out_user");
+  await page.getByTestId("password").fill("secret_sauce");
+  await page.getByTestId("login-button").click();
+
+  const errorMsg = await page.getByTestId('error').textContent()
+  await expect(errorMsg).toContain('Sorry, this user has been locked out.')
+
+
+  
+});
